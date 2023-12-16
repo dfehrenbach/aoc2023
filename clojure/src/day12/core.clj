a30311e66b842c8e8aafd02e427246f85bede782
Daniel Fehrenbach <dfehrenbach04@gmail.com>
2023-12-15 23:32:15 -0600

    Finish day 12

--- clojure/src/day12/core.clj
+++ clojure/src/day12/core.clj
@@ -1,11 +1,9 @@
 (ns day12.core
-  (:require [clojure.string :as string]
-            [clojure.set :as set]
-            [clojure.math.combinatorics :as combo]))
+  (:require [clojure.string :as string]))
 
 (defn parse-line [line]
   (let [[springs groups] (string/split line #" ")
-        groups (mapv read-string (re-seq #"[0-9]+" groups))]
+        groups (map read-string (re-seq #"[0-9]+" groups))]
     {:springs springs
      :groups groups}))
 
@@ -21,69 +19,52 @@
 (def input
   (get-input "src/day12/input.txt"))
 
-(defn find-arrangements
-  ([{:keys [springs groups]}]
-   (remove nil? (flatten (find-arrangements (vec (string/split (str springs ".") #"")) groups [] 0))))
-
-  ([springs groups arrangement groupcnt]
-   (if (empty? springs)
-
-     (if (and (empty? groups) (zero? groupcnt))
-       (apply str arrangement)
-       nil)
-
-     (let [character (first springs)
-           group (or (first groups) 0)]
-       (cond
-         (< group groupcnt) nil
-
-           ;; if we've capped a group, start looking for the next gruop
-           ;; otherwise, add the character to the arrangement and continue
-         (= character ".") (if (pos? groupcnt)
-                             (if (not= groupcnt group) nil
-                                 (find-arrangements (vec (rest springs)) (rest groups) (conj arrangement character) 0))
-                             (find-arrangements (vec (rest springs)) groups (conj arrangement character) 0))
-
-           ;; if we have exceeded the size of the current group, stop, it's invalid
-           ;; otherwise, add the character to the arrangement and continue
-         (= character "#") (find-arrangements (vec (rest springs)) groups (conj arrangement character) (inc groupcnt))
-
-           ;; try filling it in with a # or a .
-         (= character "?") (map #(find-arrangements (vec (concat [%] (vec (rest springs)))) groups arrangement groupcnt)
-                                ["." "#"])
-
-         :else nil)))))
+(def find-arrangements
+  (memoize (fn [springs groups groupcnt]
+             (if (empty? springs)
+               (if (and (empty? groups) (zero? groupcnt))
+                 1 0)
+               (let [character (first springs)
+                     group (or (first groups) 0)]
+                 (+
+                  (if (some #{"#" "?"} [character])
+                    (find-arrangements (rest springs) groups (inc groupcnt))
+                    0)
+                  (if (some #{"." "?"} [character])
+                    (if (pos? groupcnt)
+                      (if (not= groupcnt group) 0
+                          (find-arrangements (rest springs) (rest groups) 0))
+                      (find-arrangements (rest springs) groups 0))
+                    0)))))))
 
 (defn part1 [input]
   (->> input
-       (map find-arrangements)
-       (map count)
+       (pmap (fn [{:keys [springs groups]}]
+               (find-arrangements (string/split (str springs ".") #"") groups 0)))
        (reduce +)))
 
 (defn expand-input [input]
   (->> input
        (mapv (fn [{:keys [springs groups]}]
-               {:springs (apply str (interpose "?" (repeat 5 springs)))
-                :groups (vec (apply concat (repeat 5 groups)))}))))
+               {:springs (string/join "?" (repeat 5 springs))
+                :groups (vec (flatten (repeat 5 groups)))}))))
 
 (defn part2 [input]
   (->> input
        expand-input
-       (map (fn [line]
-              (let [arrangements (find-arrangements line)]
-                (prn (count arrangements))
-                (count arrangements))))
+       (pmap (fn [{:keys [springs groups]}]
+               (find-arrangements (string/split (str springs ".") #"") groups 0)))
        (reduce +)))
 
 (comment
   (first test-input)
   ;; => {:springs "???.###", :groups [1 1 3]}
 
-  (find-arrangements (first test-input))
-  ;; => ("#.#.###")
+  (find-arrangements (string/split (str (:springs (first test-input)) ".") #"") (:groups (first test-input)) 0)
+  ;; => 1
 
-  (find-arrangements (second test-input))
-  ;; => ("..#...#...###." "..#..#....###." ".#....#...###." ".#...#....###.")
+  (find-arrangements (string/split (str (:springs (second test-input)) ".") #"") (:groups (second test-input)) 0)
+  ;; => 4
 
   (part1 test-input)
   ;; => 21
@@ -91,16 +72,10 @@
   (part1 input)
   ;; => 7204
 
-  (vec (apply concat (repeat 5 (:groups (first test-input)))))
-
-  (expand-input test-input)
-
-  (count (find-arrangements (nth (part2 test-input) 5)))
-
-;; solutiontime
   (part2 test-input)
   ;; => 525152
 
   (part2 input)
+  ;; => 1672318386674
 
   0)
